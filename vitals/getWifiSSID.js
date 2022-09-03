66cf831183f8651357f4575afea7edfba70d27cc
lorib <lori.barto@live.it>
2022-09-03 12:49:18 +0200

    remove iconv run as admin to clean folders

--- vitals/getWifiSSID.js
+++ vitals/getWifiSSID.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const { Iconv } = require('iconv');
-
 module.exports = async () => {
 	const { execa } = await import('execa');
 	const cmd = 'netsh';
@@ -10,9 +8,8 @@ module.exports = async () => {
 	const { stdout } = await execa(cmd, args, { encoding: null })
 	let ret;
 
-	const iconv = new Iconv("CP936", "UTF-8")
 	//@ts-ignore
-	ret = /^\s*SSID\s*: (.+)\s*$/gm.exec(iconv.convert(stdout.toString()));
+	ret = /^\s*SSID\s*: (.+)\s*$/gm.exec(stdout);
 	ret = ret && ret.length ? ret[1] : null;
 
 	if (!ret) {
